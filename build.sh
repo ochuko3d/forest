83465aecf160692d71804ecaf2a143eafc81cae2
ochuko3d <ochuko3d@yahoo.com>
2021-09-03 11:42:22 +0100

    n

--- build.sh
+++ build.sh
@@ -6,5 +6,26 @@
 minikube start
 
 #Switch to minikube to local repository
-$(minikube docker-env:)
+eval $(minikube docker-env) 
 
+#get the file from the repository
+mkdir forest
+cd forest
+wget https://raw.githubusercontent.com/ochuko3d/forest/master/requirements.txt
+wget https://raw.githubusercontent.com/ochuko3d/forest/master/tree.py
+wget https://raw.githubusercontent.com/ochuko3d/forest/master/Dockerfile
+
+#run the build 
+docker build . -t ecosia/apple
+
+cd ..
+mkdir deploy
+cd deploy
+
+wget https://raw.githubusercontent.com/ochuko3d/forest/master/Deployment.yaml
+wget https://raw.githubusercontent.com/ochuko3d/forest/master/Ingress.yaml
+
+#Enable Minikube Service for forest
+minikube service forest-service
+#Enable Minikube Nginx Ingress
+minikube addons enable ingress
