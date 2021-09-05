38d6d0c8bdfd65185fc890ab6c62d4111f6ec0fe
ochuko <ochuko3d@yahoo.com>
2021-09-05 21:04:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
-# A Script to tell you my favorite tree
-
-
+# A Script to tell you my favorite tree.
 #Start the minikube if not running
 minikube start
-
-#Switch to minikube to local repository
-eval $(minikube docker-env) 
-
-#get the file from the repository
-mkdir forest
-cd forest
+#This makes the directory and various sub directories for the application and the deployment files, get the files from the repository
+mkdir ecosia
+cd ecosia
 mkdir trees
 cd trees
-wget https://raw.githubusercontent.com/ochuko3d/forest/master/requirements.txt
-wget https://raw.githubusercontent.com/ochuko3d/forest/master/tree.py
-wget https://raw.githubusercontent.com/ochuko3d/forest/master/Dockerfile
-wget https://raw.githubusercontent.com/ochuko3d/forest/master/test_tree.py
+#This will pull dwn the files from the repositories
 
-#run the build 
+#The requirements needed to be install
+sudo wget https://raw.githubusercontent.com/ochuko3d/forest/master/requirements.txt
+#The main application file
+sudo wget https://raw.githubusercontent.com/ochuko3d/forest/master/tree.py
+#The dockerfile needed for the build
+sudo wget https://raw.githubusercontent.com/ochuko3d/forest/master/Dockerfile
+#The python test that works with pytest
+sudo wget https://raw.githubusercontent.com/ochuko3d/forest/master/test_tree.py
+
+#This line Switches to minikube to local repository
+eval $(minikube docker-env) 
+
+#This line runs the build 
 docker build . -t ecosia/apple
 
-#pull the deployment files
+#This line Switches to minikube to local repository, I did this again just to be extra safe
+eval $(minikube docker-env) 
+
+cd ..
+
+#This makes the subdirectory for the deployment files
+mkdir deployment
+cd deployment
+#This line pulls the deployment files
 sudo wget https://raw.githubusercontent.com/ochuko3d/forest/master/Deployment.yaml
 sudo wget https://raw.githubusercontent.com/ochuko3d/forest/master/Ingress.yaml
 
-#launch the deployment and ingress
+#This line launches the deployment(consisting of the the deployment and service) and ingress
 kubectl create -f Deployment.yaml
 kubectl create -f Ingress.yaml
+
 #Enable Minikube Service for forest
 minikube service forest-service
-#Enable Minikube Nginx Ingress
+#This line Enables Minikube Nginx Ingress
 minikube addons enable ingress
 
-# Add to host file
+# This line Adds the url to host file
 echo "$(minikube ip) local.ecosia.org" | sudo tee -a /etc/hosts
